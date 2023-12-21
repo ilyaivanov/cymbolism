00f4a080cb926f911e4be0fb7fc72e00a3f11884
Ilya Ivanov <static.ila@gmail.com>
2023-12-21 21:09:18 +0200

    Add icon to an exe file

--- run.bat
+++ run.bat
@@ -3,11 +3,17 @@ if exist build rmdir /s /q build
 mkdir build
 pushd build
 
+
+copy ..\res\resources.rc .\resources.rc >> NUL
+rc /nologo .\resources.rc 
+
 set CompilerOptions=/nologo /MD /Zi
 
-set LinkerOptions=user32.lib gdi32.lib winmm.lib dwmapi.lib
+set LinkerOptions=user32.lib gdi32.lib winmm.lib dwmapi.lib 
 
-cl %CompilerOptions% ..\win.c %LinkerOptions%
+cl %CompilerOptions% ..\win.c .\resources.res %LinkerOptions% >> NUL
+
+echo Compiled
 
 set arg1=%1
 IF NOT "%arg1%" == "b" (
