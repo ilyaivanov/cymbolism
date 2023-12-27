4c83bf557e41c5bcbc19e7abc04214574dd26691
Ilya Ivanov <static.ila@gmail.com>
2023-12-27 05:30:51 +0200

    Add a playground area

--- run.bat
+++ run.bat
@@ -13,19 +13,25 @@ set LinkerOptions=user32.lib gdi32.lib winmm.lib dwmapi.lib
 IF "%arg1%" == "u" (
     cl %CompilerOptions% ..\unit.c %LinkerOptions%
     call .\unit.exe
+    GOTO end
 )
 
-IF NOT "%arg1%" == "u" (
-    copy ..\res\resources.rc .\resources.rc >> NUL
-    rc /nologo .\resources.rc 
-
-    cl %CompilerOptions% ..\win.c .\resources.res %LinkerOptions%
-
-    echo Compiled
-
-    IF NOT "%arg1%" == "b" (
-        call .\win.exe
-    )
+IF "%arg1%" == "p" (
+    cl %CompilerOptions% ..\play.c %LinkerOptions%
+    call .\play.exe
+    GOTO end
 )
 
+copy ..\res\resources.rc .\resources.rc >> NUL
+rc /nologo .\resources.rc 
+
+cl %CompilerOptions% ..\win.c .\resources.res %LinkerOptions%
+
+echo Compiled
+
+IF NOT "%arg1%" == "b" (
+    call .\win.exe
+)
+
+:end
 popd
